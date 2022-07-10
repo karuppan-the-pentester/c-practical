9474ee0d2cfc9104df0c9d6745544dd54e1c65cc
Karuppasamy K <karuppan.the.pentester@gmail.com>
2022-07-11 00:49:48 +0530

    5.A]Sting.c

---
+++ 5.A]Sting.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str1[]="Hacker ",str2[]="Tamizhan";
+    printf("The length of str1: %d \n", strlen(str1));
+    printf("The concatination of Str1+Str2: %s", strcat(str1,str2));
+
+}
